405909487db47f7a6dd21181c6613b39aa0b20cf
Jon Yurek <jyurek@thoughtbot.com>
2018-06-04 14:01:52 -0400

    commented instructons

--- .bash/dot.bash
+++ .bash/dot.bash
@@ -1,3 +1,9 @@
 alias dot="git --git-dir=$HOME/.dot --work-tree=$HOME"
 DOTCMD="git --git-dir=$HOME/.dot --work-tree=$HOME"
 dot config --local status.showUntrackedFiles no
+
+# alias dot="git --git-dir=$HOME/.dot --work-tree=$HOME"
+# cd ~
+# dot clone --bare git@github.com:jyurek/dot.git .dot
+# dot checkout
+# source .bashrc
