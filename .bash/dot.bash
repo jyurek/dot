44fba5930dc8ee060967830c5ed2e623cfd95751
Jon Yurek <jyurek@thoughtbot.com>
2018-06-05 10:19:34 -0400

    Keep dot status separate

--- .bash/dot.bash
+++ .bash/dot.bash
@@ -1,6 +1,6 @@
-alias dot="git --git-dir=$HOME/.dot --work-tree=$HOME"
-DOTCMD="git --git-dir=$HOME/.dot --work-tree=$HOME"
-dot config --local status.showUntrackedFiles no
+# alias dot="git --git-dir=$HOME/.dot --work-tree=$HOME"
+# DOTCMD="git --git-dir=$HOME/.dot --work-tree=$HOME"
+# dot config --local status.showUntrackedFiles no
 
 # alias dot="git --git-dir=$HOME/.dot --work-tree=$HOME"
 # cd ~
--- .bash/prompt.bash
+++ .bash/prompt.bash
@@ -9,29 +9,21 @@ function timer_stop {
 
 trap 'timer_start' DEBUG
 
-function which_git {
-  if [[ -d "./.dot" ]]; then
-    GIT="$DOTCMD"
-  else
-    GIT="git"
-  fi
-}
-
 function parse_git_dirty {
-  # [[ $($GIT status 2> /dev/null | tail -n1) != "nothing to commit, working tree clean" ]] && echo '✏ '
-  $($GIT diff-index --quiet HEAD 2>/dev/null) && echo -n '' || echo -n '✏ '
-}
-
-function git_branch_color {
-  if [[ -d "./.dot" ]]; then
-    branch_color="\e[34m"
-  else
-    branch_color="\e[35m"
-  fi
+  # [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working tree clean" ]] && echo '✏ '
+  $(git diff-index --quiet HEAD 2>/dev/null) && echo -n '' || echo -n '✏ '
 }
 
 function parse_git_branch {
-  $GIT branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+
+function parse_dot_dirty {
+  $(dot diff-index --quiet HEAD 2>/dev/null) && echo -n '34m' || echo -n '35m'
+}
+
+function parse_dot_branch {
+  dot branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
 
 function latest_command {
@@ -42,22 +34,22 @@ function prompt_command_function
 {
   last_result=$?
   timer_stop
-  which_git
 
   last_result="\[\e[33m\]$last_result\[\e[0m\]"
   titlebar_last_command="\[\e]2;$(latest_command)\a\]"
 
   git_branch=$(parse_git_branch)
   git_dirty=$(parse_git_dirty)
-  git_branch_color
 
   git_dirty=${git_dirty:+" \[\e[31m\]$git_dirty\[\e[0m\]"}
-  git_branch=${git_branch:+" (\[${branch_color}\]${git_branch}\[\e[0m\]${git_dirty})"}
+  git_branch=${git_branch:+" (\[\e[35m\]${git_branch}\[\e[0m\]${git_dirty})"}
+  dot_dirty=$(parse_dot_dirty)
+  dot_branch=$(parse_dot_branch)
 
   # current_ruby=$(chruby | ag \\\* | cut -d" " -f 3)
   current_ruby=$(cat ~/.tool-versions | ag ruby | cut -d" " -f 2)
 
-  PS1="$last_result \[\e[32m\]${timer_show}s\[\e[0m\] \[\e[33m\]$current_ruby\[\e[0m\] \[\e[32m\]\w\[\e[0m\]$git_branch \$ "
+  PS1="$last_result \[\e[32m\]${timer_show}s\[\e[0m\] \[\e[${dot_dirty}\]${dot_branch}\[\e[0m\] \[\e[33m\]$current_ruby\[\e[0m\] \[\e[32m\]\w\[\e[0m\]$git_branch \$ "
 }
 
 export PROMPT_COMMAND=prompt_command_function
