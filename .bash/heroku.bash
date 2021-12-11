c0afff943d65a2bf7225987b74369a130b2eb4d4
Jon Yurek <jyurek@gmail.com>
2021-12-10 23:45:34 -0500

    heroku bash config

---
+++ .bash/heroku.bash
@@ -0,0 +1,2 @@
+# heroku autocomplete setup
+HEROKU_AC_BASH_SETUP_PATH=/Users/jyurek/Library/Caches/heroku/autocomplete/bash_setup && test -f $HEROKU_AC_BASH_SETUP_PATH && source $HEROKU_AC_BASH_SETUP_PATH;
